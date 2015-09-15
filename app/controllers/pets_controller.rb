ddffd18e60a8d457f3c00ab83727b62a9da151e3
Kevin Duensing <kjduensing@gmail.com>
2015-09-14 22:20:35 -0700

    moved example pet creation into spec/models/pet_spec.rb

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,10 +4,5 @@ class PetsController < ApplicationController
 	end
 
 	def show
-		@pet = Pet.new
-		@pet.name = "Penny"
-		@pet.details = "Test description"
-		@pet.breed = "Pit bull mix"
-		@pet.age = 3
 	end
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Pet, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "creates a new pet" do
+    exp_name = 'Penny'
+    exp_deets = 'Test description'
+    exp_breed = 'Pit bull mix'
+    exp_age = 3;
+
+		pet = Pet.new
+		pet.name = "Penny"
+		pet.details = "Test description"
+		pet.breed = "Pit bull mix"
+		pet.age = 3
+
+    expect(pet.name).to eq(exp_name)
+    expect(pet.details).to eq(exp_deets)
+    expect(pet.breed).to eq(exp_breed)
+    expect(pet.age).to eq(exp_age)
+  end
 end
