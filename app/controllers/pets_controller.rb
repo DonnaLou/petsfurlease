203f6a48d96a66d735ffcfd0541f72642972a4ec
Kevin Duensing <kjduensing@gmail.com>
2015-09-14 22:28:25 -0700

    added easy methods for pet controller.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,8 +1,33 @@
 class PetsController < ApplicationController
 
-	def new
+	def index
+		@pets = Pet.all
 	end
 
 	def show
+		@pet = Pet.find(params[:id])
 	end
+
+	def new
+		@pet = Pet.new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+	
+	def update
+	end
+
+	def delete
+		@pet.destroy
+	end
+
+	private
+		def pet_params
+			params.require(:person).permit(:id)
+		end
+
 end
