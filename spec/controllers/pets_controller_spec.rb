cb12e2fef0d0beb0008a3224ad819426b579b40f
Kevin Duensing <kjduensing@gmail.com>
2015-09-14 23:09:53 -0700

    Added 2 new tests to assert that pets are indeed created when POST create is requested.

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe PetsController, type: :controller do
+  def make_roger
+      Pet.create(user_id: 1, name: "Roger", species: 'Rabbit', breed: 'Cartoon', age: 36, details: 'Married to Jessica')
+  end
 
+  describe "GET show" do
+    it "redirects to the show view" do
+
+      # Setup
+      roger = make_roger
+
+      # Act
+      get :show, id: roger.id
+
+      # Assert
+      expect(response).to render_template('show')
+    end
+
+    it "assigns the new pet to the instance variable" do
+
+      # Setup
+      roger = make_roger
+
+      # Act
+      get :show, id: roger.id
+
+      expect(assigns(:pet)).to eq(roger)
+    end
+  end
 end
