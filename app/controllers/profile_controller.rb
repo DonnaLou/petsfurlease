e97a090a9d697f0ab26470a4cf542e3efaaee3d7
Donna Lou <donnalaughalot@gmail.com>
2015-09-22 00:42:40 -0700

    Add content to profile show view
    Add content to profile show view

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,9 +3,16 @@ class ProfileController < ApplicationController
   end
 
   def show
+    @profile = Profile.new
+    @profile.firstName = "Kevin"
+    @profile.lastName = "Duensing"
+    @profile.description = "description"
+    @profile.city = "testCity"
+    @profile.state = "testState"
   end
 
   def create
+
   end
 
   def new
