57f5aa47c98d0116b0536da8d28f5df7da4f25a5
Kevin Duensing <kjduensing@gmail.com>
2015-10-01 07:56:58 -0700

    Added link and named route for viewing registered pets.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :pets
   resources :profile
-  get 'profile/:id/pets' => 'profile#pets'
+  get 'profile/:id/pets' => 'profile#pets', as: :registered_pets
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
