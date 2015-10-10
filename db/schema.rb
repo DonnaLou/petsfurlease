06afb2b034cefbf3f78b4e7ed355ab5c34e5b9d8
Donna Lou <donnalaughalot@gmail.com>
2015-10-10 00:33:36 -0700

    Reposition alerts. Move Your Registered Pet link
    Add styling around alerts so its not hidden by the menu. Styling still
    needs work
    Move the Registered Pets link in the drop down so it won't error out
    when current_user profile is nil.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151003192627) do
+ActiveRecord::Schema.define(version: 20151006044932) do
 
   create_table "pets", force: :cascade do |t|
     t.string   "name"
@@ -37,8 +37,9 @@ ActiveRecord::Schema.define(version: 20151003192627) do
     t.string   "zip"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.boolean  "is_volunteer"
   end
 
   create_table "users", force: :cascade do |t|
