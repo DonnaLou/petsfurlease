bb001e1c0322fe38db5672005628bc639ad62ee2
Donna Lou <dylou88@gmail.com>
2017-04-04 18:23:45 -0700

    Implement sending message thru the profile dialog box.

--- app/assets/javascripts/messages.coffee
+++ app/assets/javascripts/messages.coffee
@@ -26,8 +26,28 @@ App.Messages =
 	postMessageError: (response) ->
 		alert("Sending message failed. Please try again later.")
 
+	sendMessageFromDialog: (event) =>
+		postMsgUrl = "/messages"
+		data = {}
+		data.body = $("textarea#messageText").val()
+		data.recipient_profile_id = $("#profile_id").val()
+		$.ajax({
+			type: "POST",
+			url: postMsgUrl,
+			data: data,
+			success: App.Messages.postMessageDialogSuccess,
+			error: App.Messages.postMessageError
+		})
+
+	postMessageDialogSuccess: (response) ->
+		alert("Message sent")
+
+
 $(document).on "click", "#inboxGrid .tabular .item", (event) =>
 	App.Messages.changeTab(event)
 
 $(document).on "click", "button#sendMessage", (event) =>
 	App.Messages.sendMessage(event)
+
+$(document).on "click", "#sendMsgDialogBtn", (event) =>
+	App.Messages.sendMessageFromDialog(event)
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,23 +4,40 @@ class MessagesController < ApplicationController
   end
 
   def create
-  	@conversation = Conversation.find(params[:conversation_id])
 
-  	if @conversation.nil? 
-			render status: 500, json: "TODO: allow composition of new messages"
-		else
-			message = Message.new
-	  	message.body = params[:body]
-	  	message.conversation = @conversation
-	  	message.user = current_user
+  	ActiveRecord::Base.transaction do
+	  	if (params[:conversation_id].present?)
+	  		conversation = Conversation.find(params[:conversation_id])
+				message = create_message(params[:body], conversation)
 
-	  	if message.valid?
-	  		message.save!
-	  		render partial:'shared/messageDisplay', locals:{:msg => message}
-  		else
-  			render status: 500, json: message.errors.full_messages
+		  	if message.valid?
+		  		message.save!
+		  		render partial:'shared/messageDisplay', locals:{:msg => message}
+	  		else
+	  			render status: 500, json: message.errors.full_messages
+		  	end
+	  	elsif (params[:recipient_profile_id].present?)
+				profile =  Profile.find(params[:recipient_profile_id])
+
+				if (!profile.nil?)
+					conversation = Conversation.between(current_user.id, profile.user.id)
+					
+					if(conversation.length < 1)
+						conversation = create_conversation profile
+					else
+						conversation = conversation.first
+					end
+
+					message = create_message(params[:body], conversation)
+		  		if message.valid?
+						message.save!
+						render status: 200, json: true
+					else
+	  				render status: 500, json: message.errors.full_messages
+					end
+				end
 	  	end
-		end
+  	end
   end
 
   def show
@@ -31,6 +48,24 @@ class MessagesController < ApplicationController
   def delete
   	@conversation = Conversation.find(params[:id])
   	@conversation.mark_delete(current_user)
+  end
+
+  private
+
+  def create_message body, conversation
+		message = Message.new
+		message.user = current_user
+		message.body = body
+		message.conversation = conversation
+		return message
+  end
+
+  def create_conversation profile
+  	conversation = Conversation.new
+		conversation.sender = current_user
+		conversation.recipient = profile.user
+		conversation.save!
+		return conversation
   end
 
 end
