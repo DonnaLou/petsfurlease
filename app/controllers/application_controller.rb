0fa13497d41de9d200e1c1b54aeacceb32a3d63b
Donna Lou <dylou88@gmail.com>
2017-04-04 18:20:27 -0700

    Fix overwriting method with parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,13 @@ class ApplicationController < ActionController::Base
   end
 
 	protected
-	  def authenticate_user!
-	    if user_signed_in?
-	      super
-	    else
-	      redirect_to "/users/sign_in", :notice => 'Please sign in to continuing.'
-	      ## if you want render 404 page
-	      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-	    end
-	  end
+  def authenticate_user!(opts={}) 
+    if user_signed_in?
+      super
+    else
+      redirect_to "/users/sign_in", :notice => 'Please sign in to continuing.'
+      ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
 end
